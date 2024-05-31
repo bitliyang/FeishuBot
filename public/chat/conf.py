6361fae54fe72b6468346107642b138f13e2a656
bitliyang <bitliyang@hotmail.com>
2024-05-31 23:37:10 +0800

    Update conf.py

--- public/chat/conf.py
+++ public/chat/conf.py
@@ -1,4 +1,4 @@
-APP_ID = "" # #你的飞书自建应用机器人的app_id
-APP_SECRET = ""  #你的飞书自建应用机器人的app_secret
-MOBILES = ""  #申请飞书的手机号
+APP_ID = "cli_a6b45711903ad00b" # #你的飞书自建应用机器人的app_id
+APP_SECRET = "XDz9tKwd93g3elXMQaif0bqYVdKEARA4"  #你的飞书自建应用机器人的app_secret
+MOBILES = "13511066153"  #申请飞书的手机号
 NGROK_URL = "" #通过ngrok获得的公网地址
